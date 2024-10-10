311db7770c499ec4f244ceb29a3dcb893161d55b
Nikoloz Buligini <nbu@templafy.com>
2024-10-10 11:14:40 +0200

    initial setup

---
+++ lua/custom/plugins/format.lua
@@ -0,0 +1,24 @@
+return {
+
+  { -- format
+    'stevearc/conform.nvim',
+    config = function()
+      require('conform').setup {
+        formatters_by_ft = {
+          lua = { 'stylua' },
+          -- You can customize some of the format options for the filetype (:help conform.format)
+          rust = { 'rustfmt', lsp_format = 'fallback' },
+          -- Conform will run the first available formatter
+          javascript = { 'eslint_d', 'prettier', stop_after_first = true },
+          typescript = { 'eslint_d', 'prettier', stop_after_first = true },
+          typescriptreact = { 'eslint_d', 'prettier', stop_after_first = true },
+        },
+        format_on_save = {
+          -- These options will be passed to conform.format()
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        },
+      }
+    end,
+  },
+}
