82e9a977fd35dbd65f5338f0e8810504b9078a6e
Nikoloz Buligini <nbu@templafy.com>
2025-02-24 09:50:13 +0100

    Update CopilotChat configuration

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -3,12 +3,11 @@ return {
     'CopilotC-Nvim/CopilotChat.nvim',
     branch = 'main',
     dependencies = {
-      { 'github/copilot.vim' }, -- or aithub/copilot.vim
-      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
     },
     build = 'make tiktoken', -- Only on MacOS or Linux
     opts = {
-      debug = true, -- Enable debugging
       -- See Configuration section for rest
     },
     -- See Commands section for default commands if you want to lazy load on them
