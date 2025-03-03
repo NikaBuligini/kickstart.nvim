80c55124df6ca77793ed4a1df87dc9b9dfd48675
Nikoloz Buligini <nbu@templafy.com>
2025-03-03 09:07:16 +0100

    Use official copilot plugin

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,15 +1,5 @@
 return {
   {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    branch = 'main',
-    dependencies = {
-      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
-      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
-    },
-    build = 'make tiktoken', -- Only on MacOS or Linux
-    opts = {
-      -- See Configuration section for rest
-    },
-    -- See Commands section for default commands if you want to lazy load on them
+    'github/copilot.vim',
   },
 }
