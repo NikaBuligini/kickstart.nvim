f837b5b84ac0f35b98698f219210f5d83a068c47
Nikoloz Buligini <nbu@templafy.com>
2025-08-15 22:24:44 +0200

    Show hidden files in live_grep

--- init.lua
+++ init.lua
@@ -480,6 +480,11 @@ require('lazy').setup({
           find_files = {
             hidden = true,
           },
+          live_grep = {
+            additional_args = function()
+              return { '--hidden' }
+            end,
+          },
         },
 
         extensions = {
