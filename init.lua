20430b6f2cc4029eca631128f01301d58e5e9445
Nikoloz Buligini <nbu@templafy.com>
2025-11-13 15:32:49 +0100

    Add oxlint LSP

--- init.lua
+++ init.lua
@@ -895,6 +895,7 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'eslint-lsp',
+        'oxlint',
         -- 'denols',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
